8b8c12c83a1bb8da0ad0c9673dbdb081d4f7301a
Lyx <yojan.bustamante36@gmail.com>
2024-12-23 19:43:13 -0500

    storing items into the database

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', [ ExampleController::class, "homepage" ]);
+Route::get ('/', [ ExampleController::class, "homepage" ]);
 Route::get ("/about", [ ExampleController::class, "aboutpage" ]);
+
+Route::post ("/register", [ UserController::class, "register" ])->name ("register");
